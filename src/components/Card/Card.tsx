e5bab8064c05acd1cdf200fdc8c80dd5acb930e6
David Lawson <david@threesig.com>
2020-10-28 21:04:28 -0600

    build interface onto Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import * as UI from './ui';
 
-function Card({isFlipped = false, stack = 0, idx = 0}) {
+interface CardProps {
+  isFlipped: boolean
+  stack: number
+  idx: number
+}
+
+const Card: React.FC<CardProps> = ({isFlipped = false, stack = 0, idx = 0}) => {
   return (
     <UI.Container {...{isFlipped, stack, idx}}>
       <UI.Front>Front</UI.Front>
