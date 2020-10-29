f6fea0c0702261611a3f6062d2c969c9f845251f
David Lawson <david@threesig.com>
2020-10-29 12:20:43 -0600

    stricter typing definitions

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import * as UI from './ui';
 
-interface CardProps {
-  isFlipped: boolean
-  stack: number
-  idx: number
+interface ICardProps {
+  name: string,
+  value: number,
+  isFlipped?: boolean
+  stack?: number
+  idx?: number
 }
 
-const Card: React.FC<CardProps> = ({isFlipped = false, stack = 0, idx = 0}) => {
+const Card: React.FC<ICardProps> = ({name, value=0, isFlipped = false, stack = 0, idx = 0}) => {
   return (
-    <UI.Container {...{isFlipped, stack, idx}}>
-      <UI.Front>Front</UI.Front>
-      <UI.Back>Back</UI.Back>
-    </UI.Container>
+    <UI.Card {...{isFlipped, stack, idx}}>
+      <UI.Front {...{name}}/>
+      <UI.Back />
+    </UI.Card>
   );
 }
 
--- src/components/Card/ui/index.ts
+++ src/components/Card/ui/index.ts
@@ -1,10 +1,15 @@
 // Card UI
 
 import styled, {keyframes, css} from 'styled-components';
+import cardBack from '../../../assets/cardBack.jpg';
 
 const cardWidth = 400;
-const cardTransitionTime = '1s';
+const cardGutter = 50;
+const cardLiftIncrement = 1;
+const cardTransitionTime = '.5s';
 const cssCardFaceCommon = css`
+  background: center center no-repeat;
+  background-size: cover;
   backface-visibility: hidden;
   border-radius: ${cardWidth/30}px;
   position: absolute;
@@ -17,13 +22,34 @@ const cssCardFaceCommon = css`
 
 
 
-export const Container = styled.div`  
+
+/**
+ * Card
+ * 
+ */
+
+interface ICardProps {
+  isFlipped?:boolean,
+  stack?:number,
+  idx?:number
+};
+const cssSetCardState = (isFlipped:boolean=false, stack:number=0, idx:number=0) => {
+  const revolve = isFlipped ? -180 : 0;
+  const commute = isFlipped ? cardWidth + cardGutter : 0;
+  const lift = idx*cardLiftIncrement;
+
+  return css`
+    transform: rotate(${revolve}deg) translate(${commute}px ${lift}px);
+  `;
+}
+export const Card = styled.div<ICardProps>`
+  ${props => cssSetCardState(props.isFlipped, props.stack, props.idx)}
   cursor: pointer;
   transform-style: preserve-3d;
   transition: transform ${cardTransitionTime};
   position: relative;
   width: ${cardWidth}px;
-  transform: rotateY(180deg);
+  
   &:hover {
     transform: rotateY(0);
   }
@@ -34,13 +60,31 @@ export const Container = styled.div`
   }
 `;
 
-export const Front = styled.div`
+
+
+
+
+/**
+ * Card Face
+ * 
+ */
+
+interface IFrontProps {
+  name: string
+}
+const cssSetCardFace = (cardName:string) => {
+  const { default: cardFront} = require(`../../../assets/cards/${cardName}.png`);
+  return css`
+    background-image: url(${cardFront});
+  `;
+}
+export const Front = styled.div<IFrontProps>`
   ${cssCardFaceCommon}
+  ${props => cssSetCardFace(props.name)}
   transform:rotateY(0deg);
-  background-color: red;
 `
 export const Back = styled.div`
   ${cssCardFaceCommon}
-  background-color: blue;
+  background-image: url(${cardBack});
   transform: rotateY(180deg);
 `
