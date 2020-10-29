b87e3c26411133445e816f4a5a3f1a1128853809
David Lawson <david@threesig.com>
2020-10-28 21:55:34 -0600

    add Card Data

---
+++ src/data/attack-modifiers.js
@@ -0,0 +1,437 @@
+export const attackModifiers = [
+  {
+    "name": "am-p-01",
+    "points": 30,
+    "xws": "amp01"
+  },
+
+  {
+    "name": "am-p-02",
+    "points": 31,
+    "xws": "amp02"
+  },
+
+  {
+    "name": "am-p-03",
+    "points": 32,
+    "xws": "amp03"
+  },
+
+  {
+    "name": "am-p-04",
+    "points": 33,
+    "xws": "amp04"
+  },
+
+  {
+    "name": "am-p-05",
+    "points": 34,
+    "xws": "amp05"
+  },
+
+  {
+    "name": "am-p-06",
+    "points": 35,
+    "xws": "amp06"
+  },
+
+  {
+    "name": "am-p-07",
+    "points": 36,
+    "xws": "amp07"
+  },
+
+  {
+    "name": "am-p-08",
+    "points": 37,
+    "xws": "amp08"
+  },
+
+  {
+    "name": "am-p-09",
+    "points": 38,
+    "xws": "amp09"
+  },
+
+  {
+    "name": "am-p-10",
+    "points": 39,
+    "xws": "amp10"
+  },
+
+  {
+    "name": "am-p-11",
+    "points": 40,
+    "xws": "amp11"
+  },
+
+  {
+    "name": "am-p-12",
+    "points": 41,
+    "xws": "amp12"
+  },
+
+  {
+    "name": "am-p-13",
+    "points": 42,
+    "xws": "amp13"
+  },
+
+  {
+    "name": "am-p-14",
+    "points": 43,
+    "xws": "amp14"
+  },
+
+  {
+    "name": "am-p-15",
+    "points": 44,
+    "xws": "amp15"
+  },
+
+  {
+    "name": "am-p-16",
+    "points": 45,
+    "xws": "amp16"
+  },
+
+  {
+    "name": "am-p-17",
+    "points": 46,
+    "xws": "amp17"
+  },
+
+  {
+    "name": "am-p-18",
+    "points": 47,
+    "xws": "amp18"
+  },
+
+  {
+    "name": "am-p-19",
+    "points": 48,
+    "xws": "amp19"
+  },
+
+  {
+    "name": "am-p-20",
+    "points": 49,
+    "xws": "amp20"
+  },
+  {
+    "name": "am-pm-01",
+    "points": 50,
+    "xws": "ampm01"
+  },
+
+  {
+    "name": "am-pm-02",
+    "points": 51,
+    "xws": "ampm02"
+  },
+
+  {
+    "name": "am-pm-03",
+    "points": 52,
+    "xws": "ampm03"
+  },
+
+  {
+    "name": "am-pm-04",
+    "points": 53,
+    "xws": "ampm04"
+  },
+
+  {
+    "name": "am-pm-05",
+    "points": 54,
+    "xws": "ampm05"
+  },
+
+  {
+    "name": "am-pm-06",
+    "points": 55,
+    "xws": "ampm06"
+  },
+
+  {
+    "name": "am-pm-07",
+    "points": 56,
+    "xws": "ampm07"
+  },
+
+  {
+    "name": "am-pm-08",
+    "points": 57,
+    "xws": "ampm08"
+  },
+
+  {
+    "name": "am-pm-09",
+    "points": 58,
+    "xws": "ampm09"
+  },
+
+  {
+    "name": "am-pm-10",
+    "points": 59,
+    "xws": "ampm10"
+  },
+
+  {
+    "name": "am-pm-11",
+    "points": 60,
+    "xws": "ampm11"
+  },
+
+  {
+    "name": "am-pm-12",
+    "points": 61,
+    "xws": "ampm12"
+  },
+
+  {
+    "name": "am-pm-13",
+    "points": 62,
+    "xws": "ampm13"
+  },
+
+  {
+    "name": "am-pm-14",
+    "points": 63,
+    "xws": "ampm14"
+  },
+
+  {
+    "name": "am-pm-15",
+    "points": 64,
+    "xws": "ampm15"
+  },
+
+  {
+    "name": "am-pm-16",
+    "points": 65,
+    "xws": "ampm16"
+  },
+
+  {
+    "name": "am-pm-17",
+    "points": 66,
+    "xws": "ampm17"
+  },
+
+  {
+    "name": "am-pm-18",
+    "points": 67,
+    "xws": "ampm18"
+  },
+
+  {
+    "name": "am-pm-19",
+    "points": 68,
+    "xws": "ampm19"
+  },
+
+  {
+    "name": "am-pm-20",
+    "points": 69,
+    "xws": "ampm20"
+  },
+
+  {
+    "name": "am-pm-21",
+    "points": 70,
+    "xws": "ampm21"
+  },
+
+  {
+    "name": "am-pm-22",
+    "points": 71,
+    "xws": "ampm22"
+  },
+
+  {
+    "name": "am-pm-23",
+    "points": 72,
+    "xws": "ampm23"
+  },
+
+  {
+    "name": "am-pm-24",
+    "points": 73,
+    "xws": "ampm24"
+  },
+
+  {
+    "name": "am-pm-25",
+    "points": 74,
+    "xws": "ampm25"
+  },
+
+  {
+    "name": "am-pm-26",
+    "points": 75,
+    "xws": "ampm26"
+  },
+
+  {
+    "name": "am-pm-27",
+    "points": 76,
+    "xws": "ampm27"
+  },
+
+  {
+    "name": "am-pm-28",
+    "points": 77,
+    "xws": "ampm28"
+  },
+
+  {
+    "name": "am-pm-29",
+    "points": 78,
+    "xws": "ampm29"
+  },
+
+  {
+    "name": "am-pm-30",
+    "points": 79,
+    "xws": "ampm30"
+  },
+
+  {
+    "name": "am-pm-31",
+    "points": 80,
+    "xws": "ampm31"
+  },
+
+  {
+    "name": "am-pm-32",
+    "points": 81,
+    "xws": "ampm32"
+  },
+
+  {
+    "name": "am-pm-33",
+    "points": 82,
+    "xws": "ampm33"
+  },
+
+  {
+    "name": "am-pm-34",
+    "points": 83,
+    "xws": "ampm34"
+  },
+
+  {
+    "name": "am-pm-35",
+    "points": 84,
+    "xws": "ampm35"
+  },
+  {
+    "name": "am-sw-01",
+    "points": 378,
+    "xws": "amsw01"
+  },
+
+  {
+    "name": "am-sw-02",
+    "points": 379,
+    "xws": "amsw02"
+  },
+
+  {
+    "name": "am-sw-03",
+    "points": 380,
+    "xws": "amsw03"
+  },
+
+  {
+    "name": "am-sw-04",
+    "points": 381,
+    "xws": "amsw04"
+  },
+
+  {
+    "name": "am-sw-05",
+    "points": 382,
+    "xws": "amsw05"
+  },
+
+  {
+    "name": "am-sw-06",
+    "points": 383,
+    "xws": "amsw06"
+  },
+
+  {
+    "name": "am-sw-07",
+    "points": 384,
+    "xws": "amsw07"
+  },
+
+  {
+    "name": "am-sw-08",
+    "points": 385,
+    "xws": "amsw08"
+  },
+
+  {
+    "name": "am-sw-09",
+    "points": 386,
+    "xws": "amsw09"
+  },
+
+  {
+    "name": "am-sw-10",
+    "points": 387,
+    "xws": "amsw10"
+  },
+
+  {
+    "name": "am-sw-11",
+    "points": 388,
+    "xws": "amsw11"
+  },
+
+  {
+    "name": "am-sw-12",
+    "points": 389,
+    "xws": "amsw12"
+  },
+
+  {
+    "name": "am-sw-13",
+    "points": 390,
+    "xws": "amsw13"
+  },
+
+  {
+    "name": "am-sw-14",
+    "points": 391,
+    "xws": "amsw14"
+  },
+
+  {
+    "name": "am-sw-15",
+    "points": 392,
+    "xws": "amsw15"
+  },
+
+  {
+    "name": "am-sw-16",
+    "points": 393,
+    "xws": "amsw16"
+  },
+
+  {
+    "name": "am-sw-17",
+    "points": 394,
+    "xws": "amsw17"
+  },
+
+  {
+    "name": "am-sw-18",
+    "points": 395,
+    "xws": "amsw18"
+  },
+]
