ec40d47ee5ce3eb87c5586a6b6c7aa8cd8c2da94
David Lawson <david@threesig.com>
2020-10-30 16:24:30 -0600

    Card manipulation methods

--- src/contexts/SessionContext.tsx
+++ src/contexts/SessionContext.tsx
@@ -1,12 +1,90 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 import {AMCards} from '../data/attack-modifiers.js';
-
+import * as util from '../util';
 import {VCardProps, CardProps} from '../global/types';
+
+
+
+  // Standard Modifier Deck
+const deckSpec:any = {
+  "p0": 6,
+  "p1": 5,
+  "m1": 5,
+  "p2": 1,
+  "m2": 1,
+  "crit": 1,
+  "null": 1,
+};
+
+
+const hand:VCardProps[] = [];
+const discard:VCardProps[] = [];
+
+for (const cardType in deckSpec) {
+  const cardSpec = AMCards.filter((card:any) => card.type===cardType)[0];
+  const cardCount = deckSpec[cardType];
+  const {name, type, effects, description} = cardSpec;
+  
+  for (let i = 0; i < cardCount; i++) {
+    hand.push({
+      id: `${type}-${i}`,
+      name,
+      effects,
+      description,
+    } as VCardProps);
+  }
+}
+
+
+type StateType = {
+  hand: VCardProps[],
+  discard: VCardProps[]
+}
+// type ActionType = {
+//   type: string,
+//   props?: object
+// }
+
+const initialState:StateType = {
+  hand,
+  discard
+};
+
+const reducer = (state:StateType, action:any) => {
+  let {hand, discard} = state;
+  switch (action.type) {
+    
+    case 'DRAW':
+      if(hand.length) {
+        discard.push(hand.pop() as CardProps);
+      }
+      
+      return {
+        ...state,
+        hand,
+        discard
+      }
+    case 'SHUFFLE':
+      
+      hand = util.shuffle([...hand, ...discard]);
+      discard = [];
+      return {
+        ...state,
+        hand,
+        discard
+      }
+    default:
+      console.error(`ACTION TYPE "${action.type}" is not recognized`);
+      return state;
+  }
+}
 
 
 
 export type IContextProps = {
   cards: CardProps[];
+  draw: any;
+  shuffle: any;
 };
 
 export type SessionProps = {
@@ -15,54 +93,39 @@ export type SessionProps = {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const SessionContext = createContext({} as IContextProps);
 export const SessionProvider = ({children}: SessionProps) => {
-
-  // Standard Modifier Deck
-  const deckSpec:any = {
-    "p0": 6,
-    "p1": 5,
-    "m1": 5,
-    "p2": 1,
-    "m2": 1,
-    "crit": 1,
-    "null": 1,
-  };
-  
-  const hand:VCardProps[] = [];
-  const discard:VCardProps[] = [];
-  
-
-
-  for (const cardType in deckSpec) {
-    const cardSpec = AMCards.filter((card:any) => card.type===cardType)[0];
-    const cardCount = deckSpec[cardType];
-    const {name, effects, description} = cardSpec;
-    for (let i = 0; i < cardCount; i++) {
-      hand.push({
-        id: `test`,
-        name,
-        effects,
-        description,
-      } as VCardProps);
-    }
-  }
-
-
-  discard.push(hand.pop() as VCardProps);
-  discard.push(hand.pop() as VCardProps);
-  discard.push(hand.pop() as VCardProps);
-  discard.push(hand.pop() as VCardProps);
-  discard.push(hand.pop() as VCardProps);
-
-
+  const [state, dispatch] = useReducer(reducer, initialState);
   const cards:CardProps[] = [
-    ...hand.map((vCard:VCardProps, i:number) => {return {...vCard, idx:i, stack:0, isFlipped:false, value:0} as CardProps}), 
-    ...discard.map((vCard:VCardProps, i:number) => {return {...vCard, idx:i, stack:1, isFlipped:true, value:0} as CardProps}), 
+    ...state.hand.map((vCard:VCardProps, i:number) => {return {...vCard, idx:i, stack:0, isFlipped:false, value:0} as CardProps}), 
+    ...state.discard.map((vCard:VCardProps, i:number) => {return {...vCard, idx:i, stack:1, isFlipped:true, value:0} as CardProps}), 
   ];
 
   const value = {
-    cards
+    cards,
+    draw: () => dispatch({type: 'DRAW'}),
+    shuffle: () => dispatch({type: 'SHUFFLE'})
   };
 
   return (
