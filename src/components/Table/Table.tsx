ee1d71fd6cbc0e88c111d35e6657d4fbc6595647
David Lawson <david@threesig.com>
2020-10-30 13:35:17 -0600

    simplify card rendering

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as util from '../../util';
 import * as UI from './ui';
 import Card from '../Card';
 
@@ -11,23 +10,14 @@ interface TableProps {
 
 export const Table:React.FC<TableProps> = ({cards}) => {
   
-  const stacks = Object.values(util.groupBy(cards, 'stack'));
-
   const renderCard = (cardProps:CardProps, i:number) => {
     return (
       <Card {...cardProps} />
     );
   };
-  const renderStack = (cards:CardProps[]) => {
-    return (
-      <UI.Stack>
-        {cards.map(renderCard as any)}
-      </UI.Stack>
-    );
-  }
   return (
     <UI.Container>
-      {stacks.map(renderStack as any)}
+      {cards.map(renderCard as any)}
     </UI.Container>
   );
 }
