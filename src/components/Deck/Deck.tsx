3893f705544dd6307859f3a3fd2d9f5370e92364
David Lawson <david@threesig.com>
2020-11-09 12:10:14 -0700

    simplify syntax

--- src/components/Deck/Deck.tsx
+++ src/components/Deck/Deck.tsx
@@ -7,11 +7,9 @@ import * as T from '../../global/types';
 
 export const Deck:React.FC<I.DeckProps> = ({cards}) => {
   
-  const renderCard = (cardProps:T.CardProps, i:number) => {
-    return (
-      <Card key={`${cardProps.id}`} {...cardProps} />
-    );
-  };
+  const renderCard = (cardProps:T.CardProps, i:number) =>
+    <Card {...{key:`${cardProps.id}`, ...cardProps}} />;
+
   return (
     <UI.Container>
       {cards.map(renderCard as any)}
