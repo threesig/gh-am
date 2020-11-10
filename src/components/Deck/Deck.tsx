1519d31921061b48308946499b3dad2727632306
David Lawson <david@threesig.com>
2020-11-09 20:58:48 -0700

    simplify Deck

--- src/components/Deck/Deck.tsx
+++ src/components/Deck/Deck.tsx
@@ -3,18 +3,11 @@ import Card from '../Card';
 
 import * as UI from './ui';
 import * as I from '../../global/interfaces';
-import * as T from '../../global/types';
 
-export const Deck:React.FC<I.DeckProps> = ({cards}) => {
-  
-  const renderCard = (cardProps:T.CardProps, i:number) =>
-    <Card {...{key:`${cardProps.id}`, ...cardProps}} />;
-
-  return (
-    <UI.Container>
-      {cards.map(renderCard as any)}
-    </UI.Container>
-  );
-}
+export const Deck:React.FC<I.DeckProps> = ({cards}) => (
+  <UI.Container>
+    {cards.map(cardProps => <Card {...{key:`${cardProps.id}`, ...cardProps}} />)}
+  </UI.Container>
+)
 
 export default Deck;
