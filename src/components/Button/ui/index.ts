00e78ab279b2e1826a4c8391eae1b5597a07f5a8
David Lawson <david@threesig.com>
2020-11-09 22:19:48 -0700

    better button styles.  Hilite styles

--- src/components/Button/ui/index.ts
+++ src/components/Button/ui/index.ts
@@ -1,5 +1,21 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+import {darken, lighten} from 'polished';
 import * as I from '../../../global/interfaces';
+
+
+const cssMaybeHilite = (hilited:boolean) => hilited && css`
+  background-color: yellow;
+  box-shadow: 0 0 10px yellow;
+  color: ${darken(0.35, '#ffff00')};
+`;
+
+
 export const Button = styled.button<I.ButtonPropsUI>`
+  ${props => cssMaybeHilite(props.hilited)};
+  border-radius: 0.138888889em;
+  transition: all .25s;
   font-family: 'Pirata One', cursive;
+  font-size: 36px;
+  width: 6em;
+  height: 2em;
 `;
