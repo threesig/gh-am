cc340f6f427152191966475284e8d10afbb1e816
David Lawson <david@threesig.com>
2020-11-12 16:22:34 -0700

    better button styles

--- src/components/Button/ui/themes.ts
+++ src/components/Button/ui/themes.ts
@@ -9,11 +9,11 @@ const baseColor = {
 }
 
 const getBackgroundColor = (baseColor:string) => lighten(0, baseColor);
-const getTextColor = (baseColor:string) => darken(.3, baseColor);
+const getTextColor = (baseColor:string) => darken(.4, baseColor);
 
 
 const defaultBackgroundColor = baseColor.default;
-const defaultTextColor = darken(.5, baseColor.default);
+const defaultTextColor = darken(.7, baseColor.default);
 
 const defaultTheme = {
   backgroundColor: [{volume: 0, value: defaultBackgroundColor}],
@@ -32,16 +32,15 @@ const buttonThemes = {
 
 let thisColor;
 
-/**
- * Volume 1
- */
+
 
 // Shuffle
 thisColor = 'yellow';
 buttonThemes.shuffle.backgroundColor.push({volume: 1, value: thisColor});
-buttonThemes.shuffle.color.push({volume: 1, value: darken(.6, thisColor)});
+buttonThemes.shuffle.color.push({volume: 1, value: darken(.3, thisColor)});
 buttonThemes.shuffle.boxShadow.push({volume: 1, value: `0 0 2rem ${thisColor}`});
 
+// TODO: Process of adding new volume styles is very repetitive.  Find way to automate this.
 
 
 
