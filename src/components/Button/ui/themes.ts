9df441d9e2f70f66455c3b21cec6ad3a0a941c3f
David Lawson <david@threesig.com>
2020-11-12 16:06:08 -0700

    overhaul button theming.
    This is a work in progress.  nice enough now for a checkpoint.

--- src/components/Button/ui/themes.ts
+++ src/components/Button/ui/themes.ts
@@ -1,56 +1,50 @@
 import {lighten, darken, rgba} from 'polished';
 
 const baseColor = {
-  default: `gray`,
+  default: `#efefef`,
   draw: `green`,
   shuffle: `yellow`,
   advantage: `blue`,
   disadvantage: `brown`,
 }
 
-const getBackgroundColor = (baseColor:string) => lighten(.4, baseColor);
-const getTextColor = (baseColor:string) => darken(.15, baseColor);
+const getBackgroundColor = (baseColor:string) => lighten(0, baseColor);
+const getTextColor = (baseColor:string) => darken(.3, baseColor);
 
-export default {
-  'default' : {
-    backgroundColor: [{volume: 0, value: getBackgroundColor(baseColor.default)}],
-    color: [{volume: 0, value: getTextColor(baseColor.default)}],
-    boxShadow: [
-      {volume: 0, value: `none`},
-      {volume: 1, value: `0 0 20px ${baseColor.default}`},
-    ],
-  },
-  'draw' : {
-    backgroundColor: [{volume: 0, value: getBackgroundColor(baseColor.draw)}],
-    color: [{volume: 0, value: getTextColor(baseColor.draw)}],
-    boxShadow: [
-      {volume: 0, value: `none`},
-      {volume: 1, value: `0 0 20px ${baseColor.draw}`},
-    ],
-  },
-  'shuffle' : {
-    backgroundColor: [{volume: 0, value: getBackgroundColor(baseColor.shuffle)}],
-    color: [{volume: 0, value: getTextColor(baseColor.shuffle)}],
-    boxShadow: [
-      {volume: 0, value: `none`},
-      {volume: 1, value: `0 0 20px ${'orange'}`},
-      {volume: 2, value: `0 0 20px ${getBackgroundColor('orange')}`},
-    ],
-  },
-  'advantage' : {
-    backgroundColor: [{volume: 0, value: getBackgroundColor(baseColor.advantage)}],
-    color: [{volume: 0, value: getTextColor(baseColor.advantage)}],
-    boxShadow: [
-      {volume: 0, value: `none`},
-      {volume: 1, value: `0 0 20px ${baseColor.advantage}`},
-    ],
-  },
-  'disadvantage' : {
-    backgroundColor: [{volume: 0, value: getBackgroundColor(baseColor.disadvantage)}],
-    color: [{volume: 0, value: getTextColor(baseColor.disadvantage)}],
-    boxShadow: [
-      {volume: 0, value: `none`},
-      {volume: 1, value: `0 0 20px ${baseColor.disadvantage}`},
-    ],
-  },
-};
+
+const defaultBackgroundColor = baseColor.default;
+const defaultTextColor = darken(.5, baseColor.default);
+
+const defaultTheme = {
+  backgroundColor: [{volume: 0, value: defaultBackgroundColor}],
+  color: [{volume: 0, value: defaultTextColor}],
+  boxShadow: [{volume: 0, value: `none`}],
+}
+
+// Set a base for all buttons
+const buttonThemes = {
+  default: defaultTheme,
+  draw: defaultTheme,
+  advantage: defaultTheme,
+  disadvantage: defaultTheme,
+  shuffle: defaultTheme,
+}
+
+let thisColor;
+
+/**
+ * Volume 1
+ */
+
+// Shuffle
+thisColor = 'yellow';
+buttonThemes.shuffle.backgroundColor.push({volume: 1, value: thisColor});
+buttonThemes.shuffle.color.push({volume: 1, value: darken(.6, thisColor)});
+buttonThemes.shuffle.boxShadow.push({volume: 1, value: `0 0 2rem ${thisColor}`});
+
+
+
+
+
+
+export default buttonThemes;
