914418330168fdd49b31107246368c9f32c0f812
David Lawson <david@threesig.com>
2020-11-14 18:09:20 -0700

    better naming

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import Button from './Button';
 
-const onClick = jest.fn();
+const mockClick = jest.fn();
 
 test('<Button />', () => {
   const {getByTestId} = render(
-    <Button callback={onClick}>Test Button</Button>
+    <Button callback={mockClick}>Test Button</Button>
   );
   const button = getByTestId('button');
 
   fireEvent.click(button);
-  expect(onClick).toHaveBeenCalledTimes(1);
+  expect(mockClick).toHaveBeenCalledTimes(1);
 });
