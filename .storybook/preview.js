1e9d036d99dfcfb281d832c47561b557e94d977d
David Lawson <david@threesig.com>
2020-11-15 13:14:01 -0700

    configure Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
-
+import React from 'react';
+import Providers from '../src/contexts/Providers';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// .storybook/preview.js
+
+
+export const decorators = [(Story) => (
+  <Providers>
+    <div style={{ margin: '3em' }}><Story/></div>
+  </Providers>
+)];
