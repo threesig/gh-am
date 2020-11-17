fcd72cbc62eb8c82218d4a1324fb29860a46a159
David Lawson <david@threesig.com>
2020-11-16 21:24:45 -0700

    add function to retrieve styles from a styled component

--- src/util.ts
+++ src/util.ts
@@ -101,3 +101,12 @@ export const performDiscardLogic = (cards:CardTypes.CardProps[], stacks:string[]
 
   return myStacks;
 }
+
+// TODO: Add strong typing.  Enforce StyledComponent as parameter type
+export const getSCStyles = (styledComponent:any) => {
+  const styledComponentClass = styledComponent().type.styledComponentId;
+  const styledComponentRoots = document.getElementsByClassName(styledComponentClass);
+  const styles = window.getComputedStyle(styledComponentRoots[0]);
+
+  return styles;
+}
