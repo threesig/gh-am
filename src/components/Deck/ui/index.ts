1dd5b5819201cc368e0e6d9e6995ea3b02cf488a
David Lawson <david@threesig.com>
2020-11-16 21:26:26 -0700

    improve Deck styles.  Add some actual size
    base size on Card dims

--- src/components/Deck/ui/index.ts
+++ src/components/Deck/ui/index.ts
@@ -1,9 +1,11 @@
-// Card UI
+// Deck UI
 
 import styled from 'styled-components';
+import {cardHeight, cardWidth, cardGutter} from "../../Card/ui/css";
+import * as U from '../../../util';
 export const Container = styled.div`  
+  height: ${U.getRems(cardHeight*2 + cardGutter)}rem;
+  width: ${U.getRems(cardWidth*2 + cardGutter)}rem;
   perspective: 120rem;
   position: relative;
 `;
-
-export const Stack = styled.div``;
