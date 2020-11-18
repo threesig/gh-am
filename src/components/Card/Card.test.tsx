56650a01d50115320717c4071321a5b9a101438f
David Lawson <david@threesig.com>
2020-11-17 21:50:38 -0700

    more StyledComponents stylerule tests

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {render, cleanup} from '@testing-library/react';
 import Card from './Card';
 import * as Mock from './mock';
-import * as U from '../../util';
 import 'jest-styled-components';
 
 
@@ -14,20 +13,23 @@ test('<Card />', () => {
   expect(queryByTestId('cardBack')).toBeTruthy();
 
   const card = getByTestId('card');
-  const cardFront = getByTestId('cardFront');
   const cardBack = getByTestId('cardBack');
 
+  // Card is not flipped
   expect(card)
     .toHaveStyleRule('transform', 'rotateY(0deg) translate(0.4rem,-1.2rem) scale(1)');
 
+  // Card has correct back image
+  expect(cardBack)
+    .toHaveStyleRule('background-image', 'url(cardBack.jpg)');
 });
 
 test('<Card /> Flipped', () => {
   const {debug, getByTestId} = render(<Card {...Mock.cardProps.flipped} />);
   const card = getByTestId('card');
 
+  // Card is flipped
   expect(card)
     .toHaveStyleRule('transform', 'rotateY(180deg) translate(-0.4rem,-1.2rem) scale(1)');
-
 
 });
