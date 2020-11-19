ec240ec5cb965e4cd8b6d2a7ef19d255f4e996fb
David Lawson <david@threesig.com>
2020-11-18 21:13:24 -0700

    basic style test for generic button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import Button from './Button';
 import * as mock from './mock';
+import 'jest-styled-components';
 
 test('<Button />', () => {
   const {getByTestId, debug} = render(
     <Button callback={mock.testClick}>{mock.buttonText}</Button>
   );
   const button = getByTestId('button');
+  expect(button).toHaveStyleRule('font-family', '\'Pirata One\',cursive');
 
   fireEvent.click(button);
   expect(mock.testClick).toHaveBeenCalledTimes(1);
+
 });
