4cae8f1890c138b205b4cc13d1e562f107bab4d5
David Lawson <david@threesig.com>
2020-11-22 12:08:22 -0700

    Better axes, labels, styling, layout

--- src/components/ChartHistogram/ChartHistogram.tsx
+++ src/components/ChartHistogram/ChartHistogram.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {lighten, rgba} from "polished";
 import {
     BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
@@ -9,33 +10,67 @@ import {flatten, groupBy} from 'lodash';
 
 
 const ChartHistogram: React.FC<I.ChartHistogram> = ({ stacksContent}) => {
-  const labels:string[] = Object.keys(groupBy(flatten(stacksContent), 'description'));
+  const cardsFlattened = flatten(stacksContent);
+  const getAdjustedVal = (card:any) => card.description==='Bless'
+                                      ? card.value + 1
+                                      : card.description==='Curse'
+                                        ? card.value - 1
+                                        : card.value;
+
+  const cardsSorted = cardsFlattened.sort((a, b) => getAdjustedVal(a) - getAdjustedVal(b))
+  const cardsGrouped = groupBy(cardsSorted, 'description');
+
+  const labels:string[] = Object.keys(cardsGrouped);
+  const maxCardCount:number = Math.max.apply(Math, labels.map((label) => cardsGrouped[label].length))
+
+  console.log(labels);
   const data = labels.map((label) => ({
     name: label,
     value: stacksContent[Stack.READY].filter((cardItem) => cardItem.description===label).length
   }))
 
-
-
-
-  console.table(data);
+  const UIColor = '#aaa';
 
   const baseFontSize = 10;
-
+  const tickFontSize = baseFontSize*1.2;
   const tickProps = {
-    fontSize: 10
+    fontSize: tickFontSize,
+    fill: UIColor
   }
+  const gridProps = {
+    stroke: UIColor
+  }
+
+
+
   return (
-    <BarChart width={300} height={150} data={data}>
+    <BarChart width={350} height={250} data={data}>
+      <CartesianGrid {...{
+        vertical: false,
+        stroke: rgba(UIColor, 0.5),
+        strokeDasharray: '1 4',
+      }} />
       <Bar dataKey="value" fill="#8884d8" />
-      <XAxis
-        angle={-45}
-        dataKey="name"
-        dy={5}
-        textAnchor="end"
-        tick={tickProps}
-        type="category"
-      />
+      <XAxis {...{
+        angle: -45,
+        axisLine: {stroke: UIColor},
+        dataKey: "name",
+        dy: 5,
+        height: tickFontSize*3.5,
+        textAnchor: "end",
+        tick: tickProps,
+        tickLine: false,
+        ticks: labels,
+        type: "category"
+      }}/>
+      <YAxis {...{
+        axisLine: false,
+        domain: [0, maxCardCount],
+        tick: tickProps,
+        tickCount: maxCardCount,
+        tickLine: false,
+        ticks: Array.from({length: maxCardCount}, (v, i) => i+1)
+      }} />
     </BarChart>
 
   );
