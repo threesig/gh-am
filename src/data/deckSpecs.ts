79e2485183bc102191c3834a1e86de89653e945b
David Lawson <david@threesig.com>
2020-12-05 10:52:43 -0700

    cleanup deck specs

--- src/data/deckSpecs.ts
+++ src/data/deckSpecs.ts
@@ -1,16 +1,16 @@
 export const Spellweaver:any = {
-  "p0": 2,
-  "p1": 9,
-  "m1": 1,
-  "p2": 1,
-  "m2": 1,
-  "crit": 1,
-  "null": 1,
-  "bless": 2,
-  "curse": 3,
+  p0: 2,
+  p1: 9,
+  m1: 1,
+  p2: 1,
+  m2: 1,
+  crit: 1,
+  null: 1,
+  bless: 2,
+  curse: 3,
 };
 
 export const CurseBless:any = {
-  "bless": 10,
-  "curse": 10,
+  bless: 10,
+  curse: 10,
 };
