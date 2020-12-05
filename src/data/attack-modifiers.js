75bfaf2243e89ebb1729f3715b6a887a2327d430
David Lawson <david@threesig.com>
2020-12-05 11:36:01 -0700

    clean up card data definition

--- src/data/attack-modifiers.js
+++ src/data/attack-modifiers.js
@@ -1,172 +1,172 @@
 export const AMCards = [
   {
-    "type": "p0",
-    "name": "am-p-01",
-    "effects": {
-      "damageMod": 0
+    type: "p0",
+    name: "am-p-01",
+    effects: {
+      damageMod: 0
     },
-    "description": "+0"
+    description: "+0"
   },
   {
-    "type": "p1",
-    "name": "am-p-07",
-    "effects": {
-      "damageMod": 1
+    type: "p1",
+    name: "am-p-07",
+    effects: {
+      damageMod: 1
     },
-    "description": "+1"
+    description: "+1"
   },
   {
-    "type": "m1",
-    "name": "am-p-12",
-    "effects": {
-      "damageMod": -1
+    type: "m1",
+    name: "am-p-12",
+    effects: {
+      damageMod: -1
     },
-    "description": "-1"
+    description: "-1"
   },
   {
-    "type": "m2",
-    "name": "am-p-17",
-    "effects": {
-      "damageMod": -2
+    type: "m2",
+    name: "am-p-17",
+    effects: {
+      damageMod: -2
     },
-    "description": "-2"
+    description: "-2"
   },
   {
-    "type": "p2",
-    "name": "am-p-18",
-    "effects": {
-      "damageMod": 2
+    type: "p2",
+    name: "am-p-18",
+    effects: {
+      damageMod: 2
     },
-    "description": "+2"
+    description: "+2"
   },
   {
-    "type": "null",
-    "name": "am-p-19",
-    "effects": {
-      "damageMod": '-',
+    type: "null",
+    name: "am-p-19",
+    effects: {
+      damageMod: '-',
     },
-    "description": "Null",
-    "shuffle": true
+    description: "Null",
+    shuffle: true
   },
   {
-    "type": "crit",
-    "name": "am-p-20",
-    "effects": {
-      "damageMod": "2x"
+    type: "crit",
+    name: "am-p-20",
+    effects: {
+      damageMod: "2x"
     },
-    "description": "Critical",
-    "shuffle": true
+    description: "Critical",
+    shuffle: true
   },
   {
-    "type": "bless",
-    "name": "am-pm-01",
-    "effects": {
-      "damageMod": "2x"
+    type: "bless",
+    name: "am-pm-01",
+    effects: {
+      damageMod: "2x"
     },
-    "description": "Bless",
-    "temporary": true,
+    description: "Bless",
+    temporary: true,
   },
   {
-    "type": "curse",
-    "name": "am-pm-21",
-    "effects": {
-      "damageMod": '-'
+    type: "curse",
+    name: "am-pm-21",
+    effects: {
+      damageMod: '-'
     },
-    "description": "Curse",
-    "temporary": true
+    description: "Curse",
+    temporary: true
   },
   {
-    "type": "p0-stun",
-    "name": "am-sw-07",
-    "effects": {
-      "damageMod": 0,
-      "condition": "Stun"
+    type: "p0-stun",
+    name: "am-sw-07",
+    effects: {
+      damageMod: 0,
+      condition: "Stun"
     },
-    "description": "+0 Stun"
+    description: "+0 Stun"
   },
   {
-    "type": "p1-fire",
-    "name": "am-sw-08",
-    "effects": {
-      "damageMod": 1,
-      "element": "Fire"
+    type: "p1-fire",
+    name: "am-sw-08",
+    effects: {
+      damageMod: 1,
+      element: "Fire"
     },
-    "description": "+1 Generate Fire"
+    description: "+1 Generate Fire"
   },
   {
-    "type": "p1-immobilize",
-    "name": "am-sw-09",
-    "effects": {
-      "damageMod": 1,
-      "condition": "Immobilize"
+    type: "p1-immobilize",
+    name: "am-sw-09",
+    effects: {
+      damageMod: 1,
+      condition: "Immobilize"
     },
-    "description": "+1 Immobilize"
+    description: "+1 Immobilize"
   },
   {
-    "type": "p1-curse",
-    "name": "am-sw-10",
-    "effects": {
-      "damageMod": 1,
-      "condition": "Cursed"
+    type: "p1-curse",
+    name: "am-sw-10",
+    effects: {
+      damageMod: 1,
+      condition: "Cursed"
     },
-    "description": "+1 Curse"
+    description: "+1 Curse"
   },
   {
-    "type": "p2-fire",
-    "name": "am-sw-11",
-    "effects": {
-      "damageMod": 2,
-      "element": "Fire"
+    type: "p2-fire",
+    name: "am-sw-11",
+    effects: {
+      damageMod: 2,
+      element: "Fire"
     },
-    "description": "+2 Generate Fire"
+    description: "+2 Generate Fire"
   },
   {
-    "type": "p2-ice",
-    "name": "am-sw-13",
-    "effects": {
-      "damageMod": 2,
-      "element": "Ice"
+    type: "p2-ice",
+    name: "am-sw-13",
+    effects: {
+      damageMod: 2,
+      element: "Ice"
     },
-    "description": "+2 Generate Ice"
+    description: "+2 Generate Ice"
   },
   {
-    "type": "p0-earth-r",
-    "name": "am-sw-15",
-    "effects": {
-      "damageMod": 0,
-      "element": "Earth",
-      "rolling": true
+    type: "p0-earth-r",
+    name: "am-sw-15",
+    effects: {
+      damageMod: 0,
+      element: "Earth",
+      rolling: true
     },
-    "description": "+0 Generate Earth - Rolling"
+    description: "+0 Generate Earth - Rolling"
   },
   {
-    "type": "p0-air-r",
-    "name": "am-sw-16",
-    "effects": {
-      "damageMod": 0,
-      "element": "Air",
-      "rolling": true
+    type: "p0-air-r",
+    name: "am-sw-16",
+    effects: {
+      damageMod: 0,
+      element: "Air",
+      rolling: true
     },
-    "description": "+0 Generate Air - Rolling"
+    description: "+0 Generate Air - Rolling"
   },
   {
-    "type": "p0-light-r",
-    "name": "am-sw-17",
-    "effects": {
-      "damageMod": 0,
-      "element": "Light",
-      "rolling": true
+    type: "p0-light-r",
+    name: "am-sw-17",
+    effects: {
+      damageMod: 0,
+      element: "Light",
+      rolling: true
     },
-    "description": "+0 Generate Light - Rolling"
+    description: "+0 Generate Light - Rolling"
   },
   {
-    "type": "p0-darkness-r",
-    "name": "am-sw-18",
-    "effects": {
-      "damageMod": 0,
-      "element": "Darkness",
-      "rolling": true
+    type: "p0-darkness-r",
+    name: "am-sw-18",
+    effects: {
+      damageMod: 0,
+      element: "Darkness",
+      rolling: true
     },
-    "description": "+0 Generate Darkness - Rolling"
+    description: "+0 Generate Darkness - Rolling"
   },
 ];
